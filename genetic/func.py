1c9ce604d657f1bc5b9a3bb6a9c9215bdbff7700
rejkowic <184675@student.pwr.wroc.pl>
2015-01-12 23:07:28 +0100

    metoda selekcji turniejowa gotowa

--- genetic/func.py
+++ genetic/func.py
@@ -2,10 +2,10 @@ from __future__ import division
 import matplotlib.pyplot as plt
 from numpy import *
 from random import randrange, shuffle
+import itertools as it
 
 
-RESOLUTION = 100.0
-
+RESOLUTION = 1000.0
 
 class Genetic:
     def __init__(self):
@@ -24,44 +24,62 @@ class Genetic:
 
         x = arange(self.minx, self.maxx, (self.maxx-self.minx)/RESOLUTION)
 
-        m, ddd = self.find_min()
+        m = self.find_min()
         extract_min = lambda d: ([p0['x'] for p0 in d], [p1['y'] for p1 in d])
 
         mx, my = extract_min(m)
-        dx, dy = extract_min(ddd)
-        plt.plot(dx, dy, 'go')
+
+        plt.subplot(211)
         plt.plot(mx, my, 'ro')
-
-
         plt.plot(x, self.f(x))
-        plt.savefig('static/img/plot.jpg')
+
+        plt.subplot(212)
+        plt.plot(range(self.gen_count), my)
+        plt.plot([0, self.gen_count-1], 2*[min(self.f(x))], 'r')
+        plt.plot([0, self.gen_count-1], 2*[my[-1]], 'g')
+
+        while True:
+            try:
+                plt.savefig('static/img/plot.jpg')
+                break
+            except:
+                pass
 
     def gen_pop(self):
         k = randrange(self.range)/self.range
         return self.minx + k * (self.maxx - self.minx)
 
-    def group(self, lst, n):
-        return zip(*[lst[i::n] for i in range(n)])
+    def group(self, x, n):
+        for i in range(0, len(x), n):
+            yield x[i:i+n]
+
+    def create_population(self, n):
+        return [{'x': self.gen_pop()} for i in range(n)]
+
+    def crossed_champs(self, champs):
+        nc = len(champs)
+        for i in range(nc-1):
+            for j in range(i+1, nc):
+                yield {'x': (champs[i]['x']+champs[j]['x'])/2.0}
+
+    def crossed_champs_list(self, champs, n):
+        return list(it.islice(self.crossed_champs(champs), n))
 
     def find_min(self):
         key = lambda x: x['y']
-        population = [{'x': self.gen_pop()} for i in range(self.pop_count)]
-        ddd = population
+        population = self.create_population(self.pop_count)
         res = []
 
         for i in range(self.gen_count):
             for p in population:
                 p['y'] = self.f(p['x'])
-            shuffle(population)
-            g = self.group(population, self.tour_size)
+
+            g = list(self.group(population, self.tour_size))
+
             champs = [min(p, key=key) for p in g]
             res.append(min(champs, key=key))
-            print res
-            population = []
-            nc = len(champs)
-            for i in range(nc):
-                for j in range(self.tour_size):
-                    population.append({'x': (champs[i]['x']+champs[(i+j+1)%nc]['x'])/2})
 
-        return res, ddd
+            population = self.crossed_champs_list(champs, self.pop_count-len(champs))
+            population += self.create_population(self.pop_count - len(population))
+        return res
 
